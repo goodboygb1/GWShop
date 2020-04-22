f7b1306c050f1818cf106baf9fda2fbbcc2a4e72
PMJs <wutthidech@hotmail.com>
2020-04-22 22:53:36 +0700

    first version

---
+++ GWShopReal/Controller/LoginController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginController.swift
+//  GWShopReal
+//
+//  Created by PMJs on 22/4/2563 BE.
+//  Copyright © 2563 PMJs. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
