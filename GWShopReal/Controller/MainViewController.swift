0a59184bfb4dd1abf3df9c2d3307274c4f810956
PMJs <wutthidech@hotmail.com>
2020-04-24 03:58:57 +0700

    Update scroll view

--- GWShopReal/Controller/MainViewController.swift
+++ GWShopReal/Controller/MainViewController.swift
@@ -12,24 +12,18 @@ class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .red
-        // Do any additional setup after loading the view.
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         tabBarController?.navigationItem.setHidesBackButton(true, animated: true)
-            tabBarController?.navigationController?.isNavigationBarHidden = true
+        tabBarController?.navigationController?.isNavigationBarHidden = true
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        tabBarController?.navigationItem.setHidesBackButton(false, animated: false)
     }
-    */
 
 }
--- GWShopReal/Controller/NotificationViewController.swift
+++ GWShopReal/Controller/NotificationViewController.swift
@@ -16,15 +16,16 @@ class NotificationViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tabBarController?.navigationItem.setHidesBackButton(true, animated: true)
+        tabBarController?.navigationController?.isNavigationBarHidden = true
     }
-    */
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        tabBarController?.navigationItem.setHidesBackButton(false, animated: false)
+    }
+     
 
 }
