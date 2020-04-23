0c137f0ac637b49e0e6c7e85ea29f37535bb7a25
PMJs <wutthidech@hotmail.com>
2020-04-24 02:25:41 +0700

    update Storyboard

---
+++ GWShopReal/Controller/NotificationViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NotificationViewController.swift
+//  GWShopReal
+//
+//  Created by PMJs on 24/4/2563 BE.
+//  Copyright © 2563 PMJs. All rights reserved.
+//
+
+import UIKit
+
+class NotificationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
