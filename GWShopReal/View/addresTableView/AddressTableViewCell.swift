027143858e823a5b1b9c4e9e5c5976d1482e20cc
Thakorn Krittayakunakorn <porinw2000@gmail.com>
2020-04-27 03:09:33 -0700

    add address collection in register page ,add show address page and add address to database WORKED!! ,add new table view.xib

---
+++ GWShopReal/View/addresTableView/AddressTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AddressTableViewCell.swift
+//  GWShopReal
+//
+//  Created by Thakorn Krittayakunakorn on 27/4/2563 BE.
+//  Copyright © 2563 PMJs. All rights reserved.
+//
+
+import UIKit
+
+class AddressTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
